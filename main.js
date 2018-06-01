1323180d540b688da6e1e57ac92906b10efbf810
See-d Team <segolen.daniel@see-d.fr>
2018-06-01 14:50:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -27,17 +27,9 @@ rexpress.allow_free_routes = true
 rexpress.router.post('/R/russia', function (request, response, next) {
 	if(!request.body){
 		response.status(400).send('No body find')
-<<<<<<< HEAD
-<<<<<<< HEAD
 	}else if(request.body['text'].length < 4){
-=======
-	else if(request.body['text'].length < 4){
->>>>>>> 9d5361fe95b0b86cb3074c1deb85ec6d3d52d5df
-=======
-	else if(request.body['text'].length < 4){
->>>>>>> 9d5361fe95b0b86cb3074c1deb85ec6d3d52d5df
 		response.status(400).send('Text length too short')
-	}else {
+	}else{
 		request.Rfunction = "cyrillize"
 		next()
 	}
